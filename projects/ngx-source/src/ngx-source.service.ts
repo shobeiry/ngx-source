8d390633260855b7e96956604efcb9df7a4d7716
sajad <mehrabisajad@gmail.com>
2020-07-27 21:45:15 +0430

    feat: add the loadSource function and rename other functions
    Add the loadSource function that can load a source at runtime
    Rename other functions

--- projects/ngx-source/src/ngx-source.service.ts
+++ projects/ngx-source/src/ngx-source.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {SourceType} from './ngx-source-type.model';
 import {ISource} from './ngx-source.model';
 
-
 declare var document: any;
 
 @Injectable({
@@ -10,58 +9,76 @@ declare var document: any;
 })
 export class NgxSourceService {
 
-    private sources: ISource[] = [];
+  private sources: ISource[];
 
-    constructor() {
+  constructor(sources: ISource[]) {
+    this.sources = [];
+    this.addSources(...sources);
+  }
+
+  public addSources(...sources: ISource[]): void {
+    sources.forEach((source: ISource) => this.addSource(source));
+  }
+
+  public addSource(source: ISource): void {
+    const notExist = !this.sources.find((existSource: ISource) => existSource.name === source.name);
+    if (notExist) {
+      this.sources.push(source);
     }
+  }
 
-    public addSource(...sources: ISource[]): void {
-        sources.forEach((source) => this.sources.push(source));
-    }
+  public loadBySourceNames(...sourceNames: string[]): Promise<any> {
+    const promises: any[] = [];
+    sourceNames.forEach((sourceName) => promises.push(this.loadBySourceName(sourceName)));
+    return Promise.all(promises);
+  }
 
-    public load(...scripts: string[]): Promise<any> {
-        const promises: any[] = [];
-        scripts.forEach((script) => promises.push(this.loadScript(script)));
-        return Promise.all(promises);
-    }
+  public loadBySourceName(sourceName: string): Promise<any> {
+    return new Promise((resolve) => {
+      const source = this.sources.find((value) => value.name === sourceName);
+      if (!source) { // resolve if not find
+        resolve({script: sourceName, loaded: false, status: 'Not Find in Source Store'});
+      } else {
+        return Promise.all([this.loadSource(source)]);
+      }
+    });
+  }
 
-    public loadScript(name: string): Promise<any> {
-        return new Promise((resolve) => {
-            const source = this.sources.find((value) => value.name === name);
-            if (!source) { // resolve if not find
-                resolve({script: name, loaded: false, status: 'Not Find in Source Store'});
-            } else if (source.loaded) { // resolve if already loaded
-                resolve({script: name, loaded: true, status: 'Already Loaded'});
-            } else {
-                let element: any;
-                if (source.type === SourceType.STYLE) { // load style
-                    element = document.createElement('link');
-                    element.rel = 'stylesheet';
-                    element.href = source.src;
-                } else { // load script
-                    element = document.createElement('script');
-                    element.type = 'text/javascript';
-                    element.src = source.src;
-                }
+  public loadSource(source: ISource): Promise<any> {
+    this.addSource(source);
+    return new Promise<any>((resolve) => {
+      if (source.loaded) { // resolve if already loaded
+        resolve({script: name, loaded: true, status: 'Already Loaded'});
+      } else {
+        let element: any;
+        if (source.type === SourceType.STYLE) { // load style
+          element = document.createElement('link');
+          element.rel = 'stylesheet';
+          element.href = source.src;
+        } else { // load script
+          element = document.createElement('script');
+          element.type = 'text/javascript';
+          element.src = source.src;
+        }
 
-                if (element.readyState) {  // IE
-                    element.onreadystatechange = () => {
-                        if (element.readyState === 'loaded' || element.readyState === 'complete') {
-                            element.onreadystatechange = null;
-                            source.loaded = true;
-                            resolve({script: name, loaded: true, status: 'Loaded'});
-                        }
-                    };
-                } else {  // Others
-                    element.onload = () => {
-                        source.loaded = true;
-                        resolve({script: name, loaded: true, status: 'Loaded'});
-                    };
-                }
-                element.onerror = () => resolve({script: name, loaded: false, status: 'Loaded'});
-                document.getElementsByTagName('head')[0].appendChild(element);
+        if (element.readyState) {  // IE
+          element.onreadystatechange = () => {
+            if (element.readyState === 'loaded' || element.readyState === 'complete') {
+              element.onreadystatechange = null;
+              source.loaded = true;
+              resolve({script: name, loaded: true, status: 'Loaded'});
             }
-        });
-    }
+          };
+        } else {  // Others
+          element.onload = () => {
+            source.loaded = true;
+            resolve({script: name, loaded: true, status: 'Loaded'});
+          };
+        }
+        element.onerror = () => resolve({script: name, loaded: false, status: 'Loaded'});
+        document.getElementsByTagName('head')[0].appendChild(element);
+      }
+    });
+  }
 
 }
