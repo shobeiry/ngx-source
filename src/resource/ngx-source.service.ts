53ad0b9c48c6cf08ce0a75126f4f1312d4e413c9
s.shobeyri <s.shobeyri@stsepehr.ir>
2025-07-20 12:10:57 +0330

    feat: support multiple source with one name

--- src/resource/ngx-source.service.ts
+++ src/resource/ngx-source.service.ts
@@ -2,11 +2,12 @@ import { inject, Injectable } from '@angular/core';
 import { SourceType } from './ngx-source-type.model';
 import { ISource } from './ngx-source.model';
 import { ISourceLoadResult } from './ngx-source-load-result.model';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable()
 export class NgxSourceService {
-  private readonly document = inject(Document);
-  private readonly sources: Map<string, ISource> = new Map();
+  private readonly document = inject(DOCUMENT);
+  private readonly sources = new Map<string, Set<ISource>>();
 
   public addSources(...sources: ISource[]): void {
     sources.forEach(source => this.addSource(source));
@@ -14,24 +15,35 @@ export class NgxSourceService {
 
   public addSource(source: ISource): void {
     if (!this.sources.has(source.name)) {
-      this.sources.set(source.name, source);
+      this.sources.set(source.name, new Set());
+    }
+
+    const sourceSet = this.sources.get(source.name)!;
+    const serialized = JSON.stringify(source);
+
+    const alreadyExists = Array.from(sourceSet).some(s => JSON.stringify(s) === serialized);
+    if (!alreadyExists) {
+      sourceSet.add(source);
     }
   }
 
   public loadBySourceNames(...sourceNames: string[]): Promise<ISourceLoadResult[]> {
-    return Promise.all(sourceNames.map(name => this.loadBySourceName(name)));
+    return Promise.all(sourceNames.flatMap(name => this.loadBySourceName(name)));
   }
 
-  public loadBySourceName(sourceName: string): Promise<ISourceLoadResult> {
-    const source = this.sources.get(sourceName);
-    if (!source) {
-      return Promise.reject({
-        script: sourceName,
-        loaded: false,
-        status: 'Not found in source store',
-      });
+  public loadBySourceName(sourceName: string): Promise<ISourceLoadResult>[] {
+    const sources = this.sources.get(sourceName);
+    if (!sources || sources.size === 0) {
+      return [
+        Promise.reject({
+          script: sourceName,
+          loaded: false,
+          status: 'Not found in source store',
+        }),
+      ];
     }
-    return this.loadSource(source);
+
+    return Array.from(sources).map(source => this.loadSource(source));
   }
 
   public loadSources(sources: ISource[]): Promise<ISourceLoadResult[]> {
@@ -41,7 +53,9 @@ export class NgxSourceService {
 
   public loadSource(source: ISource): Promise<ISourceLoadResult> {
     this.addSource(source);
-    if (source.loaded) {
+
+    if (source.loaded || this.isSourceInDOM(source)) {
+      source.loaded = true;
       return Promise.resolve({
         script: source.name,
         loaded: true,
@@ -82,4 +96,7 @@ export class NgxSourceService {
       this.document.head.appendChild(element);
     });
   }
+
+  private isSourceInDOM = (source: ISource): boolean =>
+    !!this.document.querySelector(source.type === SourceType.STYLE ? `link[href="${source.src}"]` : `script[src="${source.src}"]`);
 }
